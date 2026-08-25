06bc9673ff083ae9e496e27957ab8178497b3ac3
1xVikashtiwari <panditvikashtiwari6@gmail.com>
2026-08-25 14:44:03 +0530

    Change frontend api

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,19 +1,15 @@
 // API Calling fn ? --> fetch
 // API Calling library --> axios
 // Installing axios command: npm i axios
-
 /* fetch("http://localhost:4400/login",{
     method: "post",
     header: {"Content-Type":"application/json"},
     body: JSON.stringify(data)
 })*/
-
 // api.post("/login",data)
-
 import axios from "axios";
 const api = axios.create({
   headers: { "Content-Type": "application/json" },
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 export default api;
